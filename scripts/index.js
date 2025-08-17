5b61eac0aa211439360c0921ec0d85077070bcbd
HomarHurtado <homar.hurtado4@gmail.com>
2025-08-16 20:13:51 -0700

    Add validation to form inputs

--- scripts/index.js
+++ scripts/index.js
@@ -138,6 +138,80 @@ function handleProfileFormSubmit(evt) {
   closePopup(editProfilePopupElement);
 }
 
+// -- Mostrar y ocultar mensaje de error de validación -- //
+function showInputError(formElement, inputElement, errorMessage) {
+  const errorElement = formElement.querySelector(
+    `#popup__input-error_${inputElement.name}`
+  );
+
+  inputElement.classList.add("popup__input_type_error");
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add("popup__error_visible");
+}
+
+function hideInputError(formElement, inputElement) {
+  const errorElement = formElement.querySelector(
+    `#popup__input-error_${inputElement.name}`
+  );
+
+  inputElement.classList.remove("popup__input_type_error");
+  errorElement.textContent = "";
+  errorElement.classList.remove("popup__error_visible");
+}
+
+// -- Validación de campos de "Editar Perfil" -- //
+function checkInputValidity(formElement, inputElement) {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+}
+
+function hasInvalidInput(inputList) {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+}
+
+// -- Activar y desactivar botón de "Guardar" -- //
+function toggleButtonState(inputList, buttonElement) {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add("popup__button_disabled");
+    buttonElement.disabled = true;
+  } else {
+    buttonElement.classList.remove("popup__button_disabled");
+    buttonElement.disabled = false;
+  }
+}
+
+// Eventos para validación de formulario
+function setEventListeners(formElement) {
+  const inputList = Array.from(formElement.querySelectorAll(".popup__input"));
+  const buttonElement = formElement.querySelector(".popup__button");
+
+  toggleButtonState(inputList, buttonElement);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+}
+
+// -- Resetear validación al cerrar y abrir modal -- //
+function resetValidation(formElement) {
+  const inputList = Array.from(formElement.querySelectorAll(".popup__input"));
+  const buttonElement = formElement.querySelector(".popup__button");
+
+  inputList.forEach((inputElement) => {
+    hideInputError(formElement, inputElement);
+  });
+
+  toggleButtonState(inputList, buttonElement);
+}
+
 // --- Maneja el envío del formulario de "Agregar Tarjeta" ---
 function handleAddCardFormSubmit(evt) {
   evt.preventDefault();
@@ -167,12 +241,14 @@ initialCards.forEach((cardData) => {
 profileEditButton.addEventListener("click", () => {
   nameInput.value = profileTitle.textContent.trim();
   aboutInput.value = profileSubtitle.textContent.trim();
+  resetValidation(editProfileForm);
   openPopup(editProfilePopupElement);
 });
 
 // Evento para abrir el modal de Agregar Tarjeta
 profileAddButton.addEventListener("click", () => {
-  addCardForm.reset(); // Limpia el formulario antes de abrirlo
+  addCardForm.reset();
+  resetValidation(addCardForm);
   openPopup(addCardPopupElement);
 });
 
@@ -199,3 +275,7 @@ document.querySelectorAll(".popup").forEach((popup) => {
     }
   });
 });
+
+// -- Activar la validacion de formularios -- //
+setEventListeners(editProfileForm);
+setEventListeners(addCardForm);
